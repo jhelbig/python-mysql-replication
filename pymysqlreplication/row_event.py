15fd70f78c1648f0026b31bfc010293ba74490f3
Jake Helbig <jake@jakehelbig.com>
2021-09-13 20:33:40 -0500

    trying to get some loggin in place to debug this specific issue

--- pymysqlreplication/row_event.py
+++ pymysqlreplication/row_event.py
@@ -246,9 +246,9 @@ class RowsEvent(BinLogEvent):
         string = self.packet.read_length_coded_pascal_string(size)
         if column.character_set_name is not None:
             encoding = self.charset_to_encoding(column.character_set_name)
-            puts("=====================================================")
-            puts(encoding)
-            puts("=====================================================")
+            print("=====================================================")
+            print(encoding)
+            print("=====================================================")
             string = string.decode(encoding)
         return string
 
